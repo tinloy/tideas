c2d53eee15f186d1a5d1085caf27fd9b4b5320c0
tinloy <tinloy.w@gmail.com>
2023-06-06 00:07:10 +1200

    update style

--- apps/studio/pages/services.tsx
+++ apps/studio/pages/services.tsx
@@ -1,7 +1,6 @@
 import Layout from "../components/layout";
 import Narbar from "../components/navbar";
 import { Box, Container, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import FurnitureService from "../assets/images/3DFurniture.jpg";
 import Footer from "../components/footer";
@@ -14,28 +13,19 @@ export default function Services() {
                 <Box component={"div"} className='rawImage'></Box>
                 <Box component={"div"} className='backdrop' />
                 <Typography variant='h1' className='serviceTitle'>
-                    Our Services
+                    Our<span style={{ fontWeight: 500 }}>&nbsp;Services</span>
                 </Typography>
             </div>
             <Container maxWidth='xl'>
-                {/* <Box component='div' id='heroImg'>
-                    <Typography variant='h1'>3D Visualisation</Typography>
-                    <Typography variant='body1'>
-                        Showcase your products, designs, and ideas in beautiful, photorealistic 3D interior settings. Interior
-                        CGIs are an incredible way to achieve photorealistic imagery that can be constantly updated to fit with
-                        new launches or a rebrand of your business, all at the fraction of the cost of traditional photoshoots.
-                    </Typography>
-                </Box> */}
                 <Box component={"div"} className='serviceItem'>
                     <Box component={"div"} className='serviceImage'>
                         <Image src={FurnitureService} alt='service1' width={900} height={600} />
                     </Box>
                     <Box component={"div"} className='serviceDescription'>
                         <Typography variant='h3' className='heading'>
-                            Furniture Visualisation
+                            <span style={{ fontWeight: 600 }}>Furniture</span>&nbsp;Visualisation
                         </Typography>
-                        <br />
-                        <Typography sx={{ pr: "4rem", fontSize: "1.5rem" }}>
+                        <Typography sx={{ fontSize: "1.5rem" }}>
                             Bring your products to life and make them completely customisable to align with your brand identity
                             and streamline your marketing. Say goodbye to photoshoots and use 3D furniture visualisation to take
                             your product launches and e-commerce capabilities to the next level. With CGI rendering, the
@@ -47,10 +37,9 @@ export default function Services() {
                 <Box component={"div"} className='serviceItem'>
                     <Box component={"div"} className='serviceDescription'>
                         <Typography variant='h3' className='heading'>
-                            Interior Visualisation
+                            <span style={{ fontWeight: 600 }}>Interior</span>&nbsp;Visualisation
                         </Typography>
-                        <br />
-                        <Typography sx={{ pr: "4rem", fontSize: "1.5rem" }}>
+                        <Typography sx={{ fontSize: "1.5rem" }}>
                             Showcase your products, designs, and ideas in beautiful, photorealistic 3D interior settings.
                             Interior CGIs are an incredible way to achieve photorealistic imagery that can be constantly updated
                             to fit with new launches or a rebrand of your business, all at the fraction of the cost of
@@ -68,10 +57,9 @@ export default function Services() {
                     </Box>
                     <Box component={"div"} className='serviceDescription'>
                         <Typography variant='h3' className='heading'>
-                            Property Visualisation
+                            <span style={{ fontWeight: 600 }}>Residential</span>&nbsp;Visualisation
                         </Typography>
-                        <br />
-                        <Typography sx={{ pr: "4rem", fontSize: "1.5rem" }}>
+                        <Typography sx={{ fontSize: "1.5rem" }}>
                             Bring your products to life and make them completely customisable to align with your brand identity
                             and streamline your marketing. Say goodbye to photoshoots and use 3D furniture visualisation to take
                             your product launches and e-commerce capabilities to the next level. With CGI rendering, the
@@ -83,10 +71,9 @@ export default function Services() {
                 <Box component={"div"} className='serviceItem'>
                     <Box component={"div"} className='serviceDescription'>
                         <Typography variant='h3' className='heading'>
-                            Property Visualisation
-                        </Typography>{" "}
-                        <br />
-                        <Typography sx={{ pr: "4rem", fontSize: "1.5rem" }}>
+                            <span style={{ fontWeight: 600 }}>Residential</span>&nbsp;Visualisation
+                        </Typography>
+                        <Typography sx={{ fontSize: "1.5rem" }}>
                             Bring your products to life and make them completely customisable to align with your brand identity
                             and streamline your marketing. Say goodbye to photoshoots and use 3D furniture visualisation to take
                             your product launches and e-commerce capabilities to the next level. With CGI rendering, the
