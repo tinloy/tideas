fcccffb6d2675110a474fec126199dc9cf162360
tinloy <tinloy.w@gmail.com>
2024-02-20 20:21:13 +1300

    send message feature

--- apps/studio/components/contact.tsx
+++ apps/studio/components/contact.tsx
@@ -1,13 +1,150 @@
-import { Button, Container, Divider, Typography } from "@mui/material";
+import { Button, Container, Divider, Grid, TextField, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
+import { useState } from "react";
 
 export default function Contact() {
+    const [message, setMessage] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        company: "",
+        role: "",
+        industry: "",
+        messageBody: "",
+    });
+
+    const logMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        switch (event.currentTarget.name) {
+            case "firstName":
+                setMessage({ ...message, firstName: event.currentTarget.value });
+                break;
+            case "lastName":
+                setMessage({ ...message, lastName: event.currentTarget.value });
+                break;
+            case "email":
+                setMessage({ ...message, email: event.currentTarget.value });
+                break;
+            case "company":
+                setMessage({ ...message, company: event.currentTarget.value });
+                break;
+            case "role":
+                setMessage({ ...message, role: event.currentTarget.value });
+                break;
+            case "industry":
+                setMessage({ ...message, industry: event.currentTarget.value });
+                break;
+            case "messageBody":
+                setMessage({ ...message, messageBody: event.currentTarget.value });
+                break;
+            default:
+                break;
+        }
+    };
+
+    const sendMessage = () => {
+        const emailContent = `Hi Tinloy,<br/><br/>${message.firstName} ${message.lastName} from ${message.company} sent you a message through <b>Tinloy Design Studio</b><br /><br /><b>Role:</b> ${message.role}<br/><br/><b>Industry:</b> ${message.industry}<br/><br/><b>Message:</b> ${message.messageBody}`;
+
+        fetch("https://hb03r9i2tg.execute-api.ap-southeast-2.amazonaws.com/sendImage", {
+            mode: "no-cors",
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                senderEmail: "tinloy.w@gmail.com",
+                message: emailContent,
+            }),
+        });
+    };
+
     return (
-        <Box component={"div"} sx={{ bgcolor: "" }}>
+        <Box component={"div"}>
             <Divider sx={{ mt: 8, mb: 8 }} />
             <Container maxWidth={"xl"} sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <Typography variant='h1'>Let&apos;s talk</Typography>
-                <Button variant='contained' sx={{ mt: 4 }}>
+                <Typography variant='h1' gutterBottom>
+                    Let&apos;s talk
+                </Typography>
+                <Container maxWidth={"md"}>
+                    <Grid container spacing={4}>
+                        <Grid item xs={6}>
+                            <TextField
+                                name='firstName'
+                                required
+                                label='First name'
+                                variant='standard'
+                                fullWidth
+                                value={message.firstName}
+                                onChange={logMessage}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                name='lastName'
+                                required
+                                label='Last name'
+                                variant='standard'
+                                fullWidth
+                                value={message.lastName}
+                                onChange={logMessage}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                name='email'
+                                required
+                                label='Email'
+                                variant='standard'
+                                fullWidth
+                                value={message.email}
+                                onChange={logMessage}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                name='company'
+                                required
+                                label='Company'
+                                variant='standard'
+                                fullWidth
+                                value={message.company}
+                                onChange={logMessage}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                name='role'
+                                label='Your role'
+                                variant='standard'
+                                fullWidth
+                                value={message.role}
+                                onChange={logMessage}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                name='industry'
+                                label='Your industry'
+                                variant='standard'
+                                fullWidth
+                                value={message.industry}
+                                onChange={logMessage}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                name='messageBody'
+                                label='Message'
+                                rows={6}
+                                multiline
+                                fullWidth
+                                value={message.messageBody}
+                                onChange={logMessage}
+                            />
+                        </Grid>
+                    </Grid>
+                </Container>
+                <Button variant='contained' sx={{ mt: 4 }} onClick={sendMessage}>
                     Drop us a message
                 </Button>
             </Container>
